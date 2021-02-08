4efa15ecc411e56b34e9fbf68b188c02e53c4eeb
John Whalley <johnpwhalley@gmail.com>
2021-02-08 12:36:16 +0900

    Updates

---
+++ precache-manifest.946ab632fda8d2d7a0c281006058ba30.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25fb9248aae4ee37174f9ec2216adca",
+    "url": "/React-Keeper-App/index.html"
+  },
+  {
+    "revision": "be0dcf3a16ad4a72c513",
+    "url": "/React-Keeper-App/static/js/2.937c3e94.chunk.js"
+  },
+  {
+    "revision": "2f9f27a385a15c713cfd",
+    "url": "/React-Keeper-App/static/js/main.95d963a7.chunk.js"
+  },
+  {
+    "revision": "f771cc0b9dae67c8406e",
+    "url": "/React-Keeper-App/static/js/runtime-main.af0f5548.js"
+  }
+]);
